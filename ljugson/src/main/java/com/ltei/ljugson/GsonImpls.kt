e6f106fd30b5db2a1d12c0eb26a046b8d0a28b65
Ltei <xavier.peltier@outlook.com>
2019-08-01 12:49:11 +0200

    Update GsonImpls.kt

--- ljugson/src/main/java/com/ltei/ljugson/GsonImpls.kt
+++ ljugson/src/main/java/com/ltei/ljugson/GsonImpls.kt
@@ -3,19 +3,62 @@ package com.ltei.ljugson
 import com.google.gson.Gson
 import com.google.gson.JsonArray
 import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import com.google.gson.reflect.TypeToken
+import java.io.Reader
+import java.lang.reflect.Type
 
+/**
+ * Contains utils functions used in this impl file
+ */
+object JsonImpls {
+    inline fun <T> tryCatchNull(block: () -> T): T? = try {
+        block()
+    } catch (t: Throwable) {
+        null
+    }
 
-inline fun <reified T> Gson.fromJson(json: JsonElement): T = fromJson(json, T::class.java)
-inline fun <reified T> Gson.fromJsonArray(json: JsonArray): List<T> = json.map { fromJson<T>(it) }
+    inline fun tryCatchBool(block: () -> Unit) = try {
+        block()
+        true
+    } catch (t: Throwable) {
+        false
+    }
 
-fun JsonElement.asBooleanOrNull() = tryCatch { asBoolean }
-fun JsonElement.asIntOrNull() = tryCatch { asInt }
-fun JsonElement.asFloatOrNull() = tryCatch { asFloat }
-fun JsonElement.asDoubleOrNull() = tryCatch { asDouble }
-fun JsonElement.asStringOrNull() = tryCatch { asString }
-
-private inline fun <T> tryCatch(block: () -> T): T? = try {
-    block()
-} catch (t: Throwable) {
-    null
+    inline fun <reified T> listType(): Type = TypeToken.getParameterized(List::class.java, T::class.java).type
 }
+
+
+// Gson methods
+
+inline fun <reified T> Gson.fromJson(json: String): T = fromJson(json, T::class.java)!!
+inline fun <reified T> Gson.fromJson(json: Reader): T = fromJson(json, T::class.java)!!
+inline fun <reified T> Gson.fromJson(json: JsonElement): T = fromJson(json, T::class.java)!!
+
+inline fun <reified T> Gson.fromJsonList(json: String): List<T> = fromJson(json, JsonImpls.listType<T>())!!
+inline fun <reified T> Gson.fromJsonList(json: Reader): List<T> = fromJson(json, JsonImpls.listType<T>())!!
+inline fun <reified T> Gson.fromJsonList(json: JsonArray): List<T> = fromJson(json, JsonImpls.listType<T>())!!
+
+
+// JsonObject methods
+
+fun JsonObject.getOrNull(key: String): JsonElement? = if (has(key)) this[key] else null
+
+
+// JsonElement methods
+
+inline val JsonElement.isInt: Boolean get() = JsonImpls.tryCatchBool { asInt }
+inline val JsonElement.isLong: Boolean get() = JsonImpls.tryCatchBool { asLong }
+inline val JsonElement.isFloat: Boolean get() = JsonImpls.tryCatchBool { asFloat }
+inline val JsonElement.isDouble: Boolean get() = JsonImpls.tryCatchBool { asDouble }
+inline val JsonElement.isBoolean: Boolean get() = JsonImpls.tryCatchBool { asBoolean }
+inline val JsonElement.isString: Boolean get() = JsonImpls.tryCatchBool { asString }
+
+inline val JsonElement.asIntOrNull: Int? get() = JsonImpls.tryCatchNull { asInt }
+inline val JsonElement.asLongOrNull: Long? get() = JsonImpls.tryCatchNull { asLong }
+inline val JsonElement.asFloatOrNull: Float? get() = JsonImpls.tryCatchNull { asFloat }
+inline val JsonElement.asDoubleOrNull: Double? get() = JsonImpls.tryCatchNull { asDouble }
+inline val JsonElement.asBooleanOrNull: Boolean? get() = JsonImpls.tryCatchNull { asBoolean }
+inline val JsonElement.asStringOrNull: String? get() = JsonImpls.tryCatchNull { asString }
+inline val JsonElement.asJsonObjectOrNull: JsonObject? get() = JsonImpls.tryCatchNull { asJsonObject }
+inline val JsonElement.asJsonArrayOrNull: JsonArray? get() = JsonImpls.tryCatchNull { asJsonArray }
